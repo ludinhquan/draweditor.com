bba662875858da5daaca666dfeec78040d83b2d2
ldquan <ludinhquan@gmail.com>
2023-03-22 20:14:09 +0700

    update infra script deploy

--- infra/clean.sh
+++ infra/clean.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-NS=${2:-staging}
+NS=${1:-staging}
+SYSTEM_NS=$NS-system
 
 NS=$NS envsubst < infra/k8s/app.yml | kubectl delete -f -
-NS=$NS envsubst < infra/k8s/system.yml | kubectl delete -f -
 NS=$NS envsubst < infra/k8s/ingress.yml | kubectl delete -f -
+NS=$SYSTEM_NS envsubst < infra/k8s/system.yml | kubectl delete -f -
--- infra/install.sh
+++ infra/install.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 
-NS=${2:-staging}
+NS=${1:-staging}
+SYSTEM_NS=$NS-system
+
+docker build -t draweditor .
 
 NS=$NS envsubst < infra/k8s/app.yml | kubectl apply -f -
-NS=$NS envsubst < infra/k8s/system.yml | kubectl apply -f -
+NS=$SYSTEM_NS envsubst < infra/k8s/system.yml | kubectl apply -f -
 NS=$NS envsubst < infra/k8s/ingress.yml | kubectl apply -f -
 
-helm install mongodb infra/helm -f infra/helm/value.yaml --namespace system --debug
+helm install mongodb infra/helm -f infra/helm/value.yaml --namespace $SYSTEM_NS
