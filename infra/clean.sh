de0203226b8b0f19e4d874e9290b7e9ae7e67269
ludinhquan <ludinhquan@gmail.com>
2023-03-29 14:45:39 +0700

    update build

--- infra/clean.sh
+++ infra/clean.sh
@@ -3,6 +3,6 @@
 NS=${1:-staging}
 SYSTEM_NS=$NS-system
 
-NS=$NS envsubst < infra/k8s/app.yml | kubectl delete -f -
+NS=$NS envsubst < infra/k8s/api.yml | kubectl delete -f -
 NS=$NS envsubst < infra/k8s/ingress.yml | kubectl delete -f -
 NS=$SYSTEM_NS envsubst < infra/k8s/system.yml | kubectl delete -f -
--- infra/install.sh
+++ infra/install.sh
@@ -3,9 +3,10 @@
 NS=${1:-staging}
 SYSTEM_NS=$NS-system
 
-docker build -t draweditor .
+docker build -t io.api.health .
 
-NS=$NS envsubst < infra/k8s/app.yml | kubectl apply -f -
+NS=$NS envsubst < infra/k8s/api.yml | kubectl apply -f -
+NS=$NS envsubst < infra/k8s/web.yml | kubectl apply -f -
 NS=$SYSTEM_NS envsubst < infra/k8s/system.yml | kubectl apply -f -
 NS=$NS envsubst < infra/k8s/ingress.yml | kubectl apply -f -
 
